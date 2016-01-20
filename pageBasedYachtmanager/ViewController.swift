06bbff1d1746f342adc3c149c8bf691e04cee9ea
reinierdlp <reinier@releaz.nl>
2016-01-20 09:56:57 +0100

    Initial Commit

---
+++ pageBasedYachtmanager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pageBasedYachtmanager
+//
+//  Created by Reinier on 20-01-16.
+//  Copyright © 2016 Reinier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
